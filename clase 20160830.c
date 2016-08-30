5a7feae7f9c591866cb2139589c96e86941b04d0
David Gomez <davidfelgomezr@hotmail.com>
2016-08-30 08:37:31 -0500

    programa en c

---
+++ clase 20160830.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+int c;
+int d;
+printf("Inserte 2 numeros");
+scanf("%d",&d);
+scanf("%d",&c);
+printf("los numeros que usted ingresó fueron:%d %d\n",c,d);
+c=d*c;
+printf("la multiplicacion da como resultado:%d\n",c);
+}
+
+
