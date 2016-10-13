9373c09119f3238dd32f7f24e82f3e1b7f3de966
David Gomez <davidfelgomezr@hotmail.com>
2016-10-13 07:23:04 -0500

    primero python

---
+++ primeroparcial.py
@@ -0,0 +1,14 @@
+a=int(input("Escriba el primer valor: "))
+b=int(input("Escriba el segundo valor: "))
+c=int(input("Escriba el tercer valor: "))
+d=int(input("Escriba el cuarto valor: "))
+e=int(input("Escriba el quinto valor: "))
+
+f=int(a+b+c+d+e)
+
+print("La sumatoria es igual: "+str(f))
+
+input
+
+Prom=(f/5)
+print("El promedio es: "+str(Prom))
