66de2a268b650bfb38181b31209aadb66b13a49a
David Gomez <davidfelgomezr@hotmail.com>
2016-10-13 07:23:33 -0500

    segundo python

---
+++ segundoparcial.py
@@ -0,0 +1,16 @@
+r=int(input("Escriba el valor del radio: "))
+
+d=int(r + r)
+
+print("El valor del diametro es: "+str(d))
+
+input
+
+c=(3.1415926*d)
+print("El valor de la circunferencia es: "+str(c))
+
+input
+
+a=(3.1415926*r*r)
+print("El valor de el area es: "+str(a))
+
